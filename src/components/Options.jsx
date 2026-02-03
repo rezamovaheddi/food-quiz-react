51c3a632657d56260a5f9e350ba313f26cd85e51
Reza <rmwhdy8@gmail.com>
2026-02-03 10:35:17 +0330

    points

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
-export default function Options({ question, dispatch }) {
+export default function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
   return (
     <div className="options">
       {question.options.map((option, index) => (
         <button
-          onClick={() => dispatch({ type: "newAnwser", payload: index })}
-          className="btn options  btn-option"
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+          className={`btn options  btn-option 
+          ${index === answer ? "answer" : ""}
+          ${hasAnswered ? (index === question.correctOption ? "correct" : "wrong") : ""}`}
           key={option}
+          disabled={hasAnswered}
         >
           {option}
         </button>
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Options from "./Options";
-export default function Question({ question, dispatch, anwser }) {
+export default function Question({ question, dispatch, answer }) {
   console.log(question);
   return (
     <div>
       <h4>{question.question}</h4>
-      <Options question={question} dispatch={dispatch} anwser={anwser} />
+      <Options question={question} dispatch={dispatch} answer={answer} />
     </div>
   );
 }
